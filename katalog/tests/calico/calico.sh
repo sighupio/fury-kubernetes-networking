2de01d6911566905b3af9505bd7fd216fe708d44
Ángel Barrera Sánchez <angel@sighup.io>
2020-09-08 13:06:19 +0200

    Add some global network policies tests

--- katalog/tests/calico/calico.sh
+++ katalog/tests/calico/calico.sh
@@ -26,7 +26,7 @@ load ./../helper
     test() {
         kubectl get pods -l k8s-app=calico-kube-controllers -o json -n kube-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
     }
-    loop_it test 30 2
+    loop_it test 30 3
     status=${loop_it_result}
     [ "$status" -eq 0 ]
 }
@@ -36,7 +36,7 @@ load ./../helper
     test() {
         kubectl get pods -l k8s-app=calico-node -o json -n kube-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
     }
-    loop_it test 30 2
+    loop_it test 30 3
     status=${loop_it_result}
     [ "$status" -eq 0 ]
 }
@@ -58,4 +58,94 @@ load ./../helper
     }
     run test
     [ "$status" -eq 0 ]
+}
+
+@test "Apply whitelist-system-ns GlobalNetworkPolicy" {
+    info
+    install() {
+        kubectl apply -f examples/globalnetworkpolicies/1.whitelist-system-namespace.yml
+    }
+    run install
+    [ "$status" -eq 0 ]
+}
+
+@test "Create a non-whitelisted namespace with an app" {
+    info
+    install() {
+        kubectl create ns test-1
+        kubectl apply -f katalog/tests/calico/resources/echo-server.yaml -n test-1
+        kubectl wait -n test-1 --for=condition=ready --timeout=120s pod -l app=echoserver
+    }
+    run install
+    [ "$status" -eq 0 ]
+}
+
+@test "Test app within the same namespace" {
+    info
+    test() {
+        kubectl create job -n test-1 isolated-test --image travelping/nettools -- curl http://echoserver.test-1.svc.cluster.local
+        kubectl wait -n test-1 --for=condition=complete --timeout=30s job/isolated-test
+    }
+    run test
+    [ "$status" -eq 0 ]
+}
+
+@test "Test app from a system namespace" {
+    info
+    test() {
+        kubectl create job -n kube-system isolated-test --image travelping/nettools -- curl http://echoserver.test-1.svc.cluster.local
+        kubectl wait -n kube-system --for=condition=complete --timeout=30s job/isolated-test
+    }
+    run test
+    [ "$status" -eq 0 ]
+}
+
+@test "Test app from a different namespace" {
+    info
+    test() {
+        kubectl create ns test-1-1
+        kubectl create job -n test-1-1 isolated-test --image travelping/nettools -- curl http://echoserver.test-1.svc.cluster.local
+        kubectl wait -n test-1-1 --for=condition=complete --timeout=30s job/isolated-test
+    }
+    run test
+    [ "$status" -eq 0 ]
+}
+
+@test "Apply deny-all GlobalNetworkPolicy" {
+    info
+    install() {
+        kubectl apply -f examples/globalnetworkpolicies/2000.deny-all.yml
+    }
+    run install
+    [ "$status" -eq 0 ]
+}
+
+@test "Test app from the same namespace (isolated namespace)" {
+    info
+    test() {
+        kubectl create job -n test-1 isolated-test-1 --image travelping/nettools -- curl http://echoserver.test-1.svc.cluster.local
+        kubectl wait -n test-1 --for=condition=complete --timeout=30s job/isolated-test-1
+    }
+    run test
+    [ "$status" -eq 1 ]
+}
+
+@test "Test app from a system namespace (isolated namespace)" {
+    info
+    test() {
+        kubectl create job -n kube-system isolated-test-1 --image travelping/nettools -- curl http://echoserver.test-1.svc.cluster.local
+        kubectl wait -n kube-system --for=condition=complete --timeout=30s job/isolated-test-1
+    }
+    run test
+    [ "$status" -eq 0 ]
+}
+
+@test "Test app from a different namespace (isolated namespace)" {
+    info
+    test() {
+        kubectl create job -n test-1-1 isolated-test-1 --image travelping/nettools -- curl http://echoserver.test-1.svc.cluster.local
+        kubectl wait -n test-1-1 --for=condition=complete --timeout=30s job/isolated-test-1
+    }
+    run test
+    [ "$status" -eq 1 ]
 }
